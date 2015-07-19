f3021496cd5244efba19497c28325548fa988119
Rob Nugen <rob@robnugen.com>
2015-07-19 09:16:36 +0900

    Failed trying to get cloud code to grap total pushups

---
+++ README.md
@@ -0,0 +1,27 @@
+this aint working
+
+
+command line: 
+
+    parse deploy
+
+    curl -X POST \
+    -H "X-Parse-Application-Id: xMCz6KllCi8Ric3MPqhIqeaW06Fb7dG8jGaxz3cv" \
+    -H "X-Parse-REST-API-Key: DSPE2bgSbotUNIekMCfMBqvPndUUxM5KoepnpTpq" \
+    -H "Content-Type: application/json"  -d '{}' \
+    https://api.parse.com/1/functions/hello
+
+    curl -X POST \
+    -H "X-Parse-Application-Id: xMCz6KllCi8Ric3MPqhIqeaW06Fb7dG8jGaxz3cv" \
+    -H "X-Parse-REST-API-Key: DSPE2bgSbotUNIekMCfMBqvPndUUxM5KoepnpTpq" \
+    -H "Content-Type: application/json"  -d '{}' \
+    https://api.parse.com/1/functions/generateLeaderboard
+
+docs
+
+https://parse.com/docs/js/guide#cloud-code
+
+online source
+
+https://www.parse.com/apps/pushups-counter/cloud_code
+
--- cloud/main.js
+++ cloud/main.js
@@ -2,5 +2,43 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+	var totalNosedowns = 0;
+	var query = new Parse.Query("SetOfNosedowns");
+	query.each(
+		function(result) {
+			totalNosedowns += result.get("count");
+		}, 
+		{
+			success: function() {
+				response.success(totalNosedowns);
+			},
+			error: function(error) {
+				response.success(error);
+			}
+		}
+	);
+});
+
+Parse.Cloud.define("generateLeaderboard", function(request, response) {
+	var totalNosedowns = 0;
+	var query = new Parse.Query("SetOfNosedowns");
+	query.find().then(function(results) {
+		for(var i=0; i<results.length; i++) {
+		  totalNosedowns += results[i].get("count");
+		}
+		response.success(totalNosedowns);
+	});
+})
+
+Parse.Cloud.afterSave("Comment", function(request) {
+  query = new Parse.Query("Post");
+  query.get(request.object.get("post").id, {
+    success: function(post) {
+      post.increment("comments");
+      post.save();
+    },
+    error: function(error) {
+      console.error("Got an error " + error.code + " : " + error.message);
+    }
+  });
 });
